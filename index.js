84236646bb9233609d0812441e45c6c78ecdd3bd
Gavin -10 <gavin@fallingonline.com>
2025-04-23 15:10:29 -0400

    Server added, pictures revised, site launched

---
+++ index.js
@@ -0,0 +1,21 @@
+
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'resume-front-end', 'dist', 'resume', 'browser')));
+
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, 'resume-front-end', 'dist', 'resume', 'browser', 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
---
+++ resume-front-end/.angular/cache/19.2.8/resume/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-INK4Q6MM.js";
+import "./chunk-KI2JESAX.js";
+import "./chunk-VKAERQZN.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
