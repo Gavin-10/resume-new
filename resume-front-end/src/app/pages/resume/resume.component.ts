6bd361c1ebb45d2dd1d8f453ed8104cc17bd28ae
Gavin -10 <gavin@fallingonline.com>
2025-04-30 23:23:02 -0400

    Resume Page Layout Complete

--- resume-front-end/src/app/pages/resume/resume.component.ts
+++ resume-front-end/src/app/pages/resume/resume.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-resume',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatCardModule, MatDividerModule, MatListModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
