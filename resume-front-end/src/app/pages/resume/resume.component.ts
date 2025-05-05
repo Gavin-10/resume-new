cd6b9c065f42c12b62fef769d1d6ab9d57e3a8e6
Gavin -10 <gavin@fallingonline.com>
2025-05-04 23:20:05 -0400

    Data added and restructured
    Resume data added and json files broken up and organized into resume
    folder in resouces

--- resume-front-end/src/app/pages/resume/resume.component.ts
+++ resume-front-end/src/app/pages/resume/resume.component.ts
@@ -1,26 +1,28 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
-import { SiteCardComponent } from '../../components/site-card/site-card.component';
 import { NgFor } from '@angular/common';
 
-import { CardInterface } from '../../interfaces/card-interface';
-import ResumeOverview from '../../Resources/resume-overview.json';
-
 import { SiteListComponent } from '../../components/site-list/site-list.component';
+import { SiteCardComponent } from '../../components/site-card/site-card.component';
+
+import { CardInterface } from '../../interfaces/card-interface';
 import { ListInterface } from '../../interfaces/list-interface';
-import ResumeSkills from '../../Resources/resume-skills.json';
+
+import Skills from '../../Resources/resume/skills.json';
+import AboutMe from '../../Resources/resume/about-me.json';
+import Summary from '../../Resources/resume/summary.json';
+import Experience from '../../Resources/resume/experience.json';
 
 @Component({
   selector: 'app-resume',
-  imports: [MatIconModule, MatCardModule, MatDividerModule, MatListModule, SiteCardComponent, NgFor, SiteListComponent],
+  imports: [MatIconModule, MatCardModule, SiteCardComponent, NgFor, SiteListComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
-  resumeOverview: CardInterface[] = ResumeOverview
-  resumeSkills: ListInterface[] = ResumeSkills;
-  test: string[] = ["First", "Second", "Third"];
+  skills: ListInterface[] = Skills;
+  aboutMe: CardInterface = AboutMe;
+  summary: CardInterface = Summary;
+  experience: CardInterface = Experience;
 }
