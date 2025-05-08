8b3682ed6cbcf83a332822f6a571a0299927d45e
Gavin -10 <gavin@fallingonline.com>
2025-05-08 16:17:58 -0400

    Contact page update
    Contact page banner added, buttons now functional

--- resume-front-end/src/app/pages/contact/contact.component.ts
+++ resume-front-end/src/app/pages/contact/contact.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { SiteCardComponent } from '../../components/site-card/site-card.component';
 
@@ -12,7 +13,22 @@ import { SiteCardComponent } from '../../components/site-card/site-card.componen
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  private snackBar = inject(MatSnackBar);
+
   email = "My Email: mcguinness52626@gmail.com"
   github = "Username: Gavin-10"
   linkedIn = "Url: linkedin.com/in/gavin-mcguinness-cs"
+
+  copyEmail() {
+    navigator.clipboard.writeText('mcguinness52626@gmail.com');
+    this.snackBar.open("Email Copied", "Dismiss")
+  }
+
+  goToGithub() {
+    window.location.href = "https://github.com/Gavin-10";
+  }
+
+  goToLinkedIn() {
+    window.location.href = "https://www.linkedin.com/in/gavin-mcguinness-cs?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BWo4zMjrOR9q%2BS33UvEGnpg%3D%3D";
+  }
 }
